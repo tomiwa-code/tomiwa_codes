1e76c313f7c91acbac7657db20f24999b8d438f3
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2024-02-09 15:51:56 +0100

    feat: Added animation to let him cook

--- src/components/general/TomiwaLoader.tsx
+++ src/components/general/TomiwaLoader.tsx
@@ -1,8 +1,26 @@
+"use client";
+import { motion } from "framer-motion";
+
 const TomiwaLoader = () => {
+  const variants = {
+    animate: {
+      scale: [0.9, 1, 0.9],
+      transition: {
+        ease: "easeInOut",
+        repeat: Infinity,
+        duration: 3,
+      },
+    },
+  };
+
   return (
-    <div className="absolute inset-0 bg-white z-[80] flex items-center justify-center">
+    <motion.div
+      variants={variants}
+      animate="animate"
+      className="absolute inset-0 bg-white z-[80] flex items-center justify-center"
+    >
       Let him cook
-    </div>
+    </motion.div>
   );
 };
 
